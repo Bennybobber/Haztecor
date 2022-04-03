f7705d867a390b5964eb8989ff326759335ccc67
Ben Sinyard <bennybobber22@gmail.com>
2022-04-03 21:14:31 +0100

    Got the Video feed onto tkinter canvas need to figure out how to put information on :)

--- haztectorGUI.py
+++ haztectorGUI.py
@@ -7,18 +7,20 @@ import rel
 import asyncio
 import websockets
 
-async def hello():
-    async with websockets.connect("ws://192.168.194.250:5000") as websocket:
-        await websocket.send("Hello world!")
-        await websocket.recv()
+#async def hello():
+#    async with websockets.connect("ws://192.168.194.250:5000") as websocket:
+#        await websocket.send("Hello world!")
+#        await websocket.recv()
 
-asyncio.run(hello())
+#asyncio.run(hello())
 
-capture = cv2.VideoCapture("https://10.65.195.238:8080/video")
+#capture = cv2.VideoCapture("https://10.65.195.238:8080/video")
+capture = cv2.VideoCapture("https://192.168.68.102:8080/video")
 
 while(True):
     _, frame = capture.read()
     cv2.imshow('livestream', frame)
+    cv2.line(frame,(0,0),(511,511),(255,0,0),5)
 
     if cv2.waitKey(1) == ord("q"):
         break
