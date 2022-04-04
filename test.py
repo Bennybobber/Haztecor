aac1822f133d7486a51c3c68a8a49cdd31b24aa8
bennybobber <bennybobber22@gmail.com>
2022-04-04 16:46:13 +0100

    Added labels, connected to webserver and stuff

--- test.py
+++ test.py
@@ -1,59 +1,137 @@
+from tkinter import *
 import tkinter
+from threading import Thread
+import serial
 import cv2
 import PIL.Image, PIL.ImageTk
 import time
- 
+import keyboard
+import websockets
+import asyncio
+import json
+
+class ZumoControl:
+    def __init__(self):
+        self.ser = serial.Serial("COM16", 9600, timeout=1)
+    
+    def keyworker(self):
+        while True:
+            key = keyboard.read_key()
+            if key == 's' or key == 'a' or key == 'w' or key == 'd':
+                self.ser.write(key.encode())
+            elif key !='w'  or  key != 'a' or key != 's' or key != 'd':
+                key = ''
+
+class SensorInput:
+    def __init__(self):
+        self.sensordict = {"cpm": '0', "temp": '0', "light": '0'}
+        self.maxTemp = "0"
+        self.lightLevel = "0"
+        self.maxCPM = "0"
+        self.dangerdict = {"cpmText": "Safe", "cpmColour": "green", "tempText": "Safe", "tempColour": "green"}
+    def setDangerLevels(self):
+        if self.maxCPM >= 50 and self.maxCPM <= 100:
+            self.dangerdict["cpmText"] = "Warning"
+            self.dangerdict["cpmColour"] = "amber"
+        elif self.maxCPM < 50 :
+            self.dangerdict["cpmText"] = "Safe"
+            self.dangerdict["cpmColour"] = "green"
+        elif self.maxCPM > 100 :
+            self.dangerdict["cpmText"] = "Danger"
+            self.dangerdict["cpmColour"] = "red"
+        
+        if self.maxTemp >= 25 and self.maxTemp <= 39:
+            self.dangerdict["tempText"] = "Warning"
+            self.dangerdict["tempColour"] = "amber"
+        elif self.maxCPM < 25 :
+            self.dangerdict["tempText"] = "Safe"
+            self.dangerdict["tempColour"] = "green"
+        elif self.maxCPM > 40 :
+            self.dangerdict["tempText"] = "Danger"
+            self.dangerdict["tempColour"] = "red"
+        
+
+
+    async def values(self, websocket):
+        async for message in websocket:
+            obj = json.loads(message)
+            self.sensordict =  obj
+            if int(obj["cpm"]) > int(self.maxCPM):
+                self.maxCPM = obj["cpm"]
+            if int(obj["temp"]) > int(self.maxTemp):
+                self.maxTemp = obj["temp"]
+            
+    async def main(self):
+        async with websockets.serve(self.values, "", 5000, ping_interval=None):
+            await asyncio.Future()
+
+    def start(self):
+        asyncio.run(self.main())
+
+        
+        
 class App:
-     def __init__(self, window, window_title, video_source=0):
-         self.window = window
-         self.window.title(window_title)
-         self.video_source = video_source
- 
-         # open video source (by default this will try to open the computer webcam)
-         self.vid = MyVideoCapture(self.video_source)
+    def __init__(self, window, window_title, video_source=0):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = video_source
+        #start thread to send zumo
+
+        self.zumo = ZumoControl()
+        self.zumothread = Thread(target=self.zumo.keyworker)
+        self.zumothread.daemon = True
+        self.zumothread.start()
+
+        self.sensors = SensorInput()
+        self.sensorthread = Thread(target=self.sensors.start)
+        self.sensorthread.daemon = True
+        self.sensorthread.start()
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = MyVideoCapture(self.video_source)
  
          # Create a canvas that can fit the above video source size
-         self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
-         self.canvas.pack()
+        self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
+        self.canvas.pack()
  
-         # Button that lets the user take a snapshot
-         self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
-         self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
+        # Button that lets the user take a snapshot
+        self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
         
-         # Rectangle that will show sexy information
-         self.canvas.create_rectangle(
-             8005, 7005, 180, 120,
-             outline="#fb0",
-             fill="#fb0"
-         )
          # After it is called once, the update method will be automatically called every delay milliseconds
-         self.delay = 1
-         self.update()
+        self.delay = 1
+        self.update()
  
-         self.window.mainloop()
+        self.window.mainloop()
  
-     def snapshot(self):
+    def snapshot(self):
          # Get a frame from the video source
-         ret, frame = self.vid.get_frame()
+        ret, frame = self.vid.get_frame()
  
-         if ret:
-             cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
  
-     def update(self):
+    def update(self):
          # Get a frame from the video source
-         ret, frame = self.vid.get_frame()
+        ret, frame = self.vid.get_frame()
  
-         if ret:
-             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
-             self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+            # Add labels
+            # print(self.sensors.sensordict)
+            Label(self.canvas,text = "Temperature: " + self.sensors.sensordict["temp"] + " degrees").place(x = 5, y = 5)  
+            Label(self.canvas,text = "Counts Per Minute: " + self.sensors.sensordict["cpm"]).place(x = 5, y = 45)
+            Label(self.canvas,text = "Light Level: " + self.sensors.sensordict["light"]).place(x = 5, y = 85)
+            Label(self.canvas,text = "Maximum temperature: " + self.sensors.maxTemp + " (" + self.sensors.dangerdict["tempText"]+")", bg=self.sensors.dangerdict["tempColour"]).place(x = 5, y = 125)
+            Label(self.canvas,text = "Maximum CPM: " + self.sensors.maxCPM + " (" + self.sensors.dangerdict["cpmText"]+")", bg=self.sensors.dangerdict["cpmColour"]).place(x = 5, y = 165)   
  
-         self.window.after(self.delay, self.update)
+        self.window.after(self.delay, self.update)
  
  
 class MyVideoCapture:
      def __init__(self, video_source=0):
          # Open the video source
-         self.vid = cv2.VideoCapture("https://192.168.68.102:8080/video")
+         self.vid = cv2.VideoCapture("https://192.168.194.169:8080/video")
          if not self.vid.isOpened():
              raise ValueError("Unable to open video source", video_source)
  
